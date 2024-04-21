d6184091b35d99d1c2face954b78e6fcaa556ad5
L Lllvvuu <git@llllvvuu.dev>
2024-04-20 22:45:42 -0700

    fix: remove duplicate handlers

--- src/extension.ts
+++ src/extension.ts
@@ -4,46 +4,16 @@
  * ------------------------------------------------------------------------------------------ */
 
 import { type ChildProcess, spawn } from "child_process"
-import type { ExtensionContext, OutputChannel, TextDocument } from "vscode"
-import { commands, languages, window, workspace } from "vscode"
+import type { ExtensionContext } from "vscode"
+import { commands, window, workspace } from "vscode"
 import type {
-  DidChangeTextDocumentParams,
-  DidCloseTextDocumentParams,
-  DidOpenTextDocumentParams,
-  DidSaveTextDocumentParams,
   LanguageClientOptions,
   ServerOptions,
 } from "vscode-languageclient/node"
 import { LanguageClient } from "vscode-languageclient/node"
-import { CompletionRequest, HoverRequest } from "vscode-languageserver-protocol"
 
 let client: LanguageClient
-let server: ChildProcess
-
-function openFile(
-  document: TextDocument,
-  languageId: string,
-  outputChannel: OutputChannel,
-  openDocuments: Set<string>,
-) {
-  if (!openDocuments.has(document.uri.toString())) {
-    openDocuments.add(document.uri.toString())
-    const param: DidOpenTextDocumentParams = {
-      textDocument: {
-        uri: document.uri.toString(),
-        languageId,
-        version: document.version,
-        text: document.getText(),
-      },
-    }
-    outputChannel.appendLine(
-      `File not opened, publishing textDocument/didOpen with param: ${JSON.stringify(
-        param,
-      )}`,
-    )
-    void client.sendNotification("textDocument/didOpen", param)
-  }
-}
+let server: ChildProcess | undefined
 
 function startServer() {
   const config = workspace.getConfiguration("glspc")
@@ -60,8 +30,7 @@ function startServer() {
     const outputChannel = window.createOutputChannel("glspc")
     outputChannel.appendLine("starting glspc...")
 
-    // eslint-disable-next-line @typescript-eslint/require-await
-    const serverOptions: ServerOptions = async (): Promise<ChildProcess> => {
+    const serverOptions: ServerOptions = (): Promise<ChildProcess> => {
       const prepend = pathPrepend?.concat(":") ?? ""
       server = spawn(serverCommand, serverCommandArguments, {
         env: {
@@ -72,7 +41,7 @@ function startServer() {
       void window.showInformationMessage(
         `Started language server: ${serverCommand}`,
       )
-      return server
+      return Promise.resolve(server)
     }
 
     const clientOptions: LanguageClientOptions = {
@@ -89,155 +58,11 @@ function startServer() {
     )
 
     void client.start().then(() => outputChannel.appendLine("started glspc."))
-
-    const openDocuments = new Set<string>()
-
-    workspace.onDidChangeTextDocument(e => {
-      if (e.document.uri.scheme !== "file") {
-        return
-      }
-      openFile(e.document, languageId, outputChannel, openDocuments)
-      const param: DidChangeTextDocumentParams = {
-        textDocument: {
-          uri: e.document.uri.toString(),
-          version: e.document.version,
-        },
-        contentChanges: e.contentChanges.map(c => ({
-          ...c,
-          range: {
-            ...c.range,
-          },
-        })),
-      }
-      outputChannel.appendLine(
-        `publishing textDocument/didChange with param: ${JSON.stringify(
-          param,
-        )}`,
-      )
-      return client.sendNotification("textDocument/didChange", param)
-    })
-    workspace.onDidSaveTextDocument(e => {
-      if (e.uri.scheme !== "file") {
-        return
-      }
-      const param: DidSaveTextDocumentParams = {
-        textDocument: {
-          uri: e.uri.toString(),
-        },
-        text: e.getText(),
-      }
-      outputChannel.appendLine(
-        `publishing textDocument/didSave with param: ${JSON.stringify(param)}`,
-      )
-      return client.sendNotification("textDocument/didSave", param)
-    })
-    workspace.onDidCloseTextDocument(e => {
-      if (e.uri.scheme !== "file") {
-        return
-      }
-      const param: DidCloseTextDocumentParams = {
-        textDocument: {
-          uri: e.uri.toString(),
-        },
-      }
-      outputChannel.appendLine(
-        `publishing textDocument/didClose with param: ${JSON.stringify(param)}`,
-      )
-      return client.sendNotification("textDocument/didClose", param)
-    })
-    workspace.onDidOpenTextDocument(e => {
-      if (e.uri.scheme !== "file") {
-        return
-      }
-      openDocuments.add(e.uri.toString())
-      const param: DidOpenTextDocumentParams = {
-        textDocument: {
-          uri: e.uri.toString(),
-          languageId,
-          version: e.version,
-          text: e.getText(),
-        },
-      }
-      outputChannel.appendLine(
-        `publishing textDocument/didOpen with param: ${JSON.stringify(param)}`,
-      )
-      return client.sendNotification("textDocument/didOpen", param)
-    })
-
-    if (client.initializeResult?.capabilities.hoverProvider) {
-      languages.registerHoverProvider([languageId], {
-        async provideHover(document, position, token) {
-          if (document.uri.scheme !== "file") {
-            outputChannel.appendLine(
-              `uri: ${document.uri.toString()} is not a file`,
-            )
-            return
-          }
-          outputChannel.appendLine(
-            `provideHover for ${document.uri.toString()}`,
-          )
-          openFile(document, languageId, outputChannel, openDocuments)
-          return client
-            .sendRequest(
-              HoverRequest.type,
-              client.code2ProtocolConverter.asTextDocumentPositionParams(
-                document,
-                position,
-              ),
-              token,
-            )
-            .then(result => {
-              if (token.isCancellationRequested) {
-                return null
-              }
-              return client.protocol2CodeConverter.asHover(result)
-            })
-        },
-      })
-    }
-
-    const triggers =
-      client.initializeResult?.capabilities.completionProvider
-        ?.triggerCharacters
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    outputChannel.appendLine(`trigger characters: ${triggers}`)
-    languages.registerCompletionItemProvider(
-      [languageId],
-      {
-        async provideCompletionItems(document, position, token) {
-          if (document.uri.scheme !== "file") {
-            outputChannel.appendLine(
-              `uri: ${document.uri.toString()} is not a file`,
-            )
-            return
-          }
-          outputChannel.appendLine(
-            `provideCompletionItems for ${document.uri.toString()}`,
-          )
-          openFile(document, languageId, outputChannel, openDocuments)
-          const result = await client.sendRequest(
-            CompletionRequest.type,
-            client.code2ProtocolConverter.asTextDocumentPositionParams(
-              document,
-              position,
-            ),
-            token,
-          )
-          if (token.isCancellationRequested) {
-            return null
-          }
-          return client.protocol2CodeConverter.asCompletionResult(result)
-        },
-      },
-      ...(triggers ?? []),
-    )
   }
 }
 
 async function killServer(): Promise<void> {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  await client?.stop()
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  await client.stop()
   server?.kill()
 }
 
